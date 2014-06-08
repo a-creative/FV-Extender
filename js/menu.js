ca69e0c652b2089b0b32aaea2c3f263acab35afe
Martin Andersen <m.andersen.post@gmail.com>
2014-06-08 02:36:03 +0200

    - Changes related to move to Google Chrome Webshop - Removal of unused menu item ("Just help") - Removal of update-menuitem because the Webshop handles this.

--- js/menu.js
+++ js/menu.js
@@ -26,14 +26,6 @@ function _settings_clicked() {
 	return false;
 }
 
-function _just_help_btn_clicked() {
-	alert( 'Feature not available yet. Just help will be added in a later version.' );
-	window.close();
-
-	return false;
-	
-}
-
 function _donate_btn_clicked() {
 	bgp.openTab( { url: "http://a-creative.dk/donate/" } );
 	window.close();
@@ -44,49 +36,9 @@ window.onload = function() {
 	
 	bgp = chrome.extension.getBackgroundPage();
 	
-	if ( bgp.is_google_shop ) {
-		var update_link = document.getElementById( 'updates-btn' );
-		update_link.innerHTML = '- FVE is up-to-date';
-	} else {
-		
-		bgp.checkForUpdates( function( update ) {
-			
-			var update_link = document.getElementById( 'updates-btn' );
-		
-			if ( update ) {
-				
-				if ( update.manual_update ) {
-					update_link.target = '_blank';
-					update_link.innerHTML = 'Update FVE';
-					update_link.title = 'Click to read more about and update FVE to the latest version: ' + update.version ;
-					
-				} else {
-					update_link.innerHTML = 'Update FVE now';
-					
-					update_link.title = 'Click to update FVE instantly to the latest version: ' + update.version;
-				}
-				
-				update_link.target = '_blank';
-				update_link.href = update.url;
-				update_link.className = 'enabled';
-				
-				var sign = document.getElementById( 'update-sign' );
-				sign.href = update.url;
-				sign.target = '_blank';
-				sign.style.display = 'block';
-				
-			} else {
-				update_link.innerHTML = '- FVE is up-to-date';
-			}
-			
-			
-		} );
-	}
-	
 	var abort_btn = document.getElementById( 'abort-btn' );
 	var process_btn = document.getElementById( 'process-btn' );
 	var settings_btn = document.getElementById( 'settings-btn' );
-	var just_help_btn = document.getElementById( 'just-help-btn' );
 	var donate_btn = document.getElementById( 'donate-btn' );
 	
 	if ( bgp.processing ) {
@@ -99,6 +51,5 @@ window.onload = function() {
 	}
 	
 	settings_btn.onclick = _settings_clicked;
-	just_help_btn.onclick = _just_help_btn_clicked;
 	donate_btn.onclick = _donate_btn_clicked;	
 };
